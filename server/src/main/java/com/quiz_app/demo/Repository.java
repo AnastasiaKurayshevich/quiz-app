d1420c69db6a51269373087a5b887b6856687e76
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-05 18:34:37 +0200

    Intansiated JpaRepo inside repo

--- server/src/main/java/com/quiz_app/demo/Repository.java
+++ server/src/main/java/com/quiz_app/demo/Repository.java
@@ -3,4 +3,9 @@ package com.quiz_app.demo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public class Repository  {
+    JpaRepository repository;
+
+    public Repository(JpaRepository repository) {
+        this.repository = repository;
+    }
 }
