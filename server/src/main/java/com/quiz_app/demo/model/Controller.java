17510ddf579b14966a470ef61543b120ef6674ce
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 10:14:00 +0200

    Instansiated service in Controller

--- server/src/main/java/com/quiz_app/demo/model/Controller.java
+++ server/src/main/java/com/quiz_app/demo/model/Controller.java
@@ -1,9 +1,20 @@
 package com.quiz_app.demo.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/question")
 public class Controller {
+
+    private final Service service;
+
+    @Autowired
+    public Controller(Service service) {
+        this.service = service;
+    }
+
+
 }
