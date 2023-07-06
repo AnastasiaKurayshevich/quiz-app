41aa1646030440529c9745e5c6f66271eb590c2b
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 16:38:52 +0200

    Created handleQuizCompletion function

--- client/src/Question/QuestionCard.tsx
+++ client/src/Question/QuestionCard.tsx
@@ -28,6 +28,10 @@ export const QuestionCard = () => {
         setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
     }
 
+    const handleQuizCompletion = () => {
+        setQuizCompleted(true);
+    }
+
 
     
   return (
