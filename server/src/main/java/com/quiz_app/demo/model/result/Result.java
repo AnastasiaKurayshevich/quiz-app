2c6cbaabe15c0826cb8c1dfe219f382609b877bb
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 19:43:03 +0200

    Setters, getters and constructors for results

--- server/src/main/java/com/quiz_app/demo/model/result/Result.java
+++ server/src/main/java/com/quiz_app/demo/model/result/Result.java
@@ -14,4 +14,38 @@ public class Result {
     private String name;
 
     private String score;
+
+    public Result(String id, String name, String score) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+
+     public Result(){
+
+     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
 }
