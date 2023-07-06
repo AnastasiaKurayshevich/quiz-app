790cfd6cf849bdc84b49d15797812a24c67fb8dd
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 10:12:37 +0200

    Added request mapping

--- server/src/main/java/com/quiz_app/demo/model/Controller.java
+++ server/src/main/java/com/quiz_app/demo/model/Controller.java
@@ -1,7 +1,9 @@
 package com.quiz_app.demo.model;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/question")
 public class Controller {
 }
