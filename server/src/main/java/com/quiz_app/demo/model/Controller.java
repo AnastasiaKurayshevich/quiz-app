c7713db7f31448457d931f606f924b81c61375a1
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 10:16:27 +0200

    Created getRandomQuestion method inside controller which will return the question with id 1 (for now) when fetching from /api/question/random

--- server/src/main/java/com/quiz_app/demo/model/Controller.java
+++ server/src/main/java/com/quiz_app/demo/model/Controller.java
@@ -2,6 +2,7 @@ package com.quiz_app.demo.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,12 @@ public class Controller {
     @Autowired
     public Controller(Service service) {
         this.service = service;
+    }
+
+    @GetMapping("/random")
+    public ResponseEntity<Question> getRandomQuestion(){
+        Question question = service.getById(1);
+        return ResponseEntity.ok(question);
     }
 
 
