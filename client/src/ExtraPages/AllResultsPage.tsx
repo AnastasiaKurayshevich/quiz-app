31900fde41086d4a7970ff62dd5f37a8a1cf5f10
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 21:02:49 +0200

    fetching data

--- client/src/ExtraPages/AllResultsPage.tsx
+++ client/src/ExtraPages/AllResultsPage.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 export const AllResultsPage = () => {
+    const [results, setResults] = useState([]);
+    useEffect(() => {
+        const fetchResults = async () => {
+          try {
+            const response = await fetch('http://localhost:3000/api/results');
+            const data = await response.json();
+            setResults(data);
+          } catch (error) {
+            console.error('Error fetching results:', error);
+          }
+        };
+    
+        fetchResults();
+      }, []);
   return (
-    <div>AllResultsPage</div>
+    <div>
+        <h2>All Results</h2>
+        <ul>
+        {results.map((result, index) => (
+          <li key={result.id}>{result.name} - {result.score}</li>
+        ))}
+      </ul>
+
+    </div>
   )
 }
