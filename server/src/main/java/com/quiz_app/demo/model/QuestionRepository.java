18c076b94fd9dc11f0e818e85e14df9a7eaf3d09
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 10:06:37 +0200

    Instansiated QuestionJpaRepo in QuestionRepo

--- server/src/main/java/com/quiz_app/demo/model/QuestionRepository.java
+++ server/src/main/java/com/quiz_app/demo/model/QuestionRepository.java
@@ -1,4 +1,14 @@
 package com.quiz_app.demo.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class QuestionRepository {
+    QuestionJpaRepository repo;
+
+    @Autowired
+    public QuestionRepository(QuestionJpaRepository repo) {
+        this.repo = repo;
+    }
 }
