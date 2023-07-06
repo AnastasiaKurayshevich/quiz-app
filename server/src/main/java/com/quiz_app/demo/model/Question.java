df166902cdc686a08fb4ee7a18d4e1da381850c3
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 09:25:05 +0200

    Created Constructor, getters and setters for Question

--- server/src/main/java/com/quiz_app/demo/model/Question.java
+++ server/src/main/java/com/quiz_app/demo/model/Question.java
@@ -14,4 +14,38 @@ public class Question {
    @OneToMany
    @JoinColumn(name="answer_id")
    private List<Answer> answers;
+
+   public Question(long id, String question, List<Answer> answers) {
+      this.id = id;
+      this.question = question;
+      this.answers = answers;
+   }
+
+   public Question() {
+
+   }
+
+   public long getId() {
+      return id;
+   }
+
+   public void setId(long id) {
+      this.id = id;
+   }
+
+   public String getQuestion() {
+      return question;
+   }
+
+   public void setQuestion(String question) {
+      this.question = question;
+   }
+
+   public List<Answer> getAnswers() {
+      return answers;
+   }
+
+   public void setAnswers(List<Answer> answers) {
+      this.answers = answers;
+   }
 }
