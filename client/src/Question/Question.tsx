aad9dd9bca4b304df5775befa91a520ee073c297
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 11:37:08 +0200

    Using useState to set whether answer should be visible or not

--- client/src/Question/Question.tsx
+++ client/src/Question/Question.tsx
@@ -1,12 +1,14 @@
+import { useState } from 'react'
 import { QuestionProp } from './type/QuestionProp'
 
 
 export const Question = (props: QuestionProp) => {
+    const [showAnswers, setShowAnswer] = useState(false); 
   return (
     <>
     <p>{props.question}</p>
-    <p>{props.answers.map(answer => 
-    <li key={answer.id}><button>{answer.answer}</button></li>)}</p>
+    <ul>{props.answers.map(answer => 
+    <li key={answer.id}><button>{answer.answer}</button></li>)}</ul>
     </>
   )
 }
