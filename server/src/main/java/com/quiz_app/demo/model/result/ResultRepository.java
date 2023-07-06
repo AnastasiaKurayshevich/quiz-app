059e10376f44b4884de57bc38deddf92fefac20d
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 19:49:44 +0200

    Initialized JpaRepo in Result repo

--- server/src/main/java/com/quiz_app/demo/model/result/ResultRepository.java
+++ server/src/main/java/com/quiz_app/demo/model/result/ResultRepository.java
@@ -1,7 +1,15 @@
 package com.quiz_app.demo.model.result;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ResultRepository {
+
+    private final ResultJpaRepository repo;
+
+    @Autowired
+    public ResultRepository(ResultJpaRepository repo) {
+        this.repo = repo;
+    }
 }
