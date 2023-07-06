44cafcfe3f40bf477c87f8d76343578693aa7f17
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 10:10:10 +0200

    Instansiated QRepo in Service layer

--- server/src/main/java/com/quiz_app/demo/model/Service.java
+++ server/src/main/java/com/quiz_app/demo/model/Service.java
@@ -1,6 +1,14 @@
 package com.quiz_app.demo.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 @org.springframework.stereotype.Service
 public class Service {
 
+    QuestionRepository repo;
+
+    @Autowired
+    public Service(QuestionRepository repo) {
+        this.repo = repo;
+    }
 }
