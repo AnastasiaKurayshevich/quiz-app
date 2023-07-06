46996419a8a081fb9b4b8bef51d645ac233452b9
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 11:03:40 +0200

    Rendering QuestionCard in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { QuestionCard } from './Question/QuestionCard'
 import TestComponent from './TestComponent'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-    <TestComponent />
+    <QuestionCard  />
     </>
   )
 }
