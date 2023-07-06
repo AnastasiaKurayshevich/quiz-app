bce672b9b4816a2c8b0a3c0e19c6c09e60b00ea1
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 09:43:26 +0200

    Inserted test data

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO question (question) VALUES ('What is 2+2');
+
+INSERT INTO answer (question_id, answer, is_correct) VALUE (1, 'No', false);
+INSERT INTO answer (question_id, answer, is_correct) VALUE (1, 'I dont know', false);
+INSERT INTO answer (question_id, answer, is_correct) VALUE (1, 'Maybe the same as 3+1?', true);
