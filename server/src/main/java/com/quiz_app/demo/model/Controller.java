3d3774c32d061b97e660491b51e969fcd4aa5e1c
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-06 11:13:38 +0200

    Added cross origin annotation to controller

--- server/src/main/java/com/quiz_app/demo/model/Controller.java
+++ server/src/main/java/com/quiz_app/demo/model/Controller.java
@@ -2,12 +2,14 @@ package com.quiz_app.demo.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/question")
+@CrossOrigin(origins = "http://127.0.0.1:5173/", allowedHeaders = "*")
 public class Controller {
 
     private final Service service;
